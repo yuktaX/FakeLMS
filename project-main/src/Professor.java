235f9520084f7e085064e1a5eef2ee47bf782b65
brij <brijdesai2003@gmail.com>
2023-04-17 22:44:39 +0530

    implemented addPerformance() and addCourse()

--- project-main/src/Professor.java
+++ project-main/src/Professor.java
@@ -1,39 +1,36 @@
-public class Professor 
-{
-    String Name;
-    String Professor_ID;
-    String Email;
-    public Professor(String name, String id, String email)
-    {
-        this.Name = name;
-        this.Professor_ID = id;
-        this.Email = email;
-    }
-    public String getName()
-    { 
-		return Name; 
+public class Professor {
+	String Name;
+	Integer Professor_ID;
+	String Email;
+
+	public Professor(String name, Integer id, String email) {
+		this.Name = name;
+		this.Professor_ID = id;
+		this.Email = email;
 	}
-	public void setName(String name)
-	{ 
-		this.Name = name; 
+
+	public String getName() {
+		return Name;
 	}
-	public String getProfessortID() 
-	{ 
-		return Professor_ID; 
+
+	public void setName(String name) {
+		this.Name = name;
 	}
-	public void setProfessor_ID(String id)
-	{ 
-		this.Professor_ID = id; 
+
+	public Integer getProfessortID() {
+		return Professor_ID;
 	}
-	public String getEmail() 
-	{ 
-		return Name; 
+
+	public void setProfessor_ID(Integer id) {
+		this.Professor_ID = id;
 	}
-	public void setEmail(String email)
-	{ 
-		this.Email = email; 
+
+	public String getEmail() {
+		return Name;
+	}
+
+	public void setEmail(String email) {
+		this.Email = email;
 	}
 
 }
-
-
--- project-main/src/StudentDAO_JDBC.java
+++ project-main/src/StudentDAO_JDBC.java
@@ -2,65 +2,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
-
 public class StudentDAO_JDBC implements StudentDAO {
-																																																																																																																																																																																																																																															Connection dbConnection;
+	Connection dbConnection;
 
-	public StudentDAO_JDBC(Connection dbconn){
+	public StudentDAO_JDBC(Connection dbconn) {
 		// JDBC driver name and database URL
- 		//  Database credentials
+		// Database credentials
 		dbConnection = dbconn;
 	}
 
 	@Override
 	public boolean getStudentByKey(String id) {
-		//Student s = new Student();
+		// Student s = new Student();
 		String sql;
 		Statement stmt = null;
-		
-		try{
+
+		try {
 			stmt = dbConnection.createStatement();
 			sql = "select Student_ID from student where STUDENT_ID = " + id;
 			ResultSet rs = stmt.executeQuery(sql);
-																																																																																																																																																																																			
-			//STEP 5: Extract data from result set
-			while(rs.next()){
-				//Retrieve by column name
-				String studentid  = rs.getString("Student_ID");
-				if(studentid == null)
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				String studentid = rs.getString("Student_ID");
+				if (studentid == null)
 					return false;
-				//String name = rs.getString("name");
-				//s.setStudentID(id);
-				//s.setName(name);
+				// String name = rs.getString("name");
+				// s.setStudentID(id);
+				// s.setName(name);
 				break;
 				// Add exception handling here if more than one row is returned
 			}
 			return true;
 		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 		// Add exception handling when there is no matching record
-		//return true;
+		// return true;
 		return false;
 	}
+
 	@Override
 	public void getEligibleCourses() {
 		String sql;
 		Statement stmt = null;
-		try{
+		try {
 			stmt = dbConnection.createStatement();
 			sql = "select CourseName from Course where STUDENT_ID = " + id;
 			ResultSet rs = stmt.executeQuery(sql);
 
-		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 	}
 
@@ -81,7 +80,5 @@ public class StudentDAO_JDBC implements StudentDAO {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'getTranscript'");
 	}
-
-	
 
 }

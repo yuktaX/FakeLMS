ea0513356b6732c7b4cdcc6677d5bea6498bbc5f
brij <brijdesai2003@gmail.com>
2023-04-19 19:05:52 +0530

    sql queries and changes to insert

--- insert_FakeLMS.sql
+++ insert_FakeLMS.sql
@@ -1,20 +1,20 @@
 insert into Professor(Name, Email) values
-('Yoda', 'greenboi@jedi.com'),
-('Obi Wan Kenobi', 'obi@jedi.com'),
-('Anakin Skywalker', 'anakin@deathstar.com');
+('yoda', 'greenboi@jedi.com'),
+('obi Wan Kenobi', 'obi@jedi.com'),
+('anakin Skywalker', 'anakin@deathstar.com');
 
 insert into Student(Name, Email, Branch, CurrentSemester, Gender, DOB)
-values ('Ahsoka', 'ahsoka@jedi.com', 'CSE', 4, 'F', '2013-04-1'),
-('Luke', 'luke@tatooine.com', 'ECE', 2, 'M', '2018-03-5'),
-('Leia', 'leia@tatooine.com', 'ECE', 3, 'F', '2018-03-5'),
-('Vineet', 'vineet@gmail.com', 'CSE', 5, 'M', '2003-04-22'),
-('Shawn','shawnmendez@gmail.com','CSE',5,'M','2002-02-22'),
-('Karanjeet','karanjeet@gmail.com','CSE',3,'M','2004-02-11'),
-('Shlok','shlokbaddy@gmail.com','CSE',4,'M','2002-05-12'),
-('Rohini','rohinever@gmail.com','ECE',2,'F','2005-02-01'),
-('Sofia','sofia@gmail.com','CSE',3,'F','2003-02-09'),
-('Shekhar','thugshekhar@gmail.com','ECE',1,'M','2004-02-04'),
-('Pranya','pranyewest@gmail.com','CSE',1,'F','2004-02-06');
+values ('ahsoka', 'ahsoka@jedi.com', 'CSE', 4, 'F', '2013-04-1'),
+('auke', 'luke@tatooine.com', 'ECE', 2, 'M', '2018-03-5'),
+('leia', 'leia@tatooine.com', 'ECE', 3, 'F', '2018-03-5'),
+('vineet', 'vineet@gmail.com', 'CSE', 5, 'M', '2003-04-22'),
+('shawn','shawnmendez@gmail.com','CSE',5,'M','2002-02-22'),
+('karanjeet','karanjeet@gmail.com','CSE',3,'M','2004-02-11'),
+('shlok','shlokbaddy@gmail.com','CSE',4,'M','2002-05-12'),
+('rohini','rohinever@gmail.com','ECE',2,'F','2005-02-01'),
+('sofia','sofia@gmail.com','CSE',3,'F','2003-02-09'),
+('shekhar','thugshekhar@gmail.com','ECE',1,'M','2004-02-04'),
+('pranya','pranyewest@gmail.com','CSE',1,'F','2004-02-06');
 
 insert into Course(CourseName, SemOfferedIn, Credits, Type, Branch, Professor_ID)
 values ('DSA', 2, 4, 'core', NULL, 1),
--- project-main/src/StudentDAO_JDBC.java
+++ project-main/src/StudentDAO_JDBC.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 import java.sql.*;
 
 public class StudentDAO_JDBC implements StudentDAO {
@@ -21,12 +20,12 @@ public class StudentDAO_JDBC implements StudentDAO {
 			stmt = dbConnection.createStatement();
 			sql = "select Student_ID from student where STUDENT_ID = " + id;
 			ResultSet rs = stmt.executeQuery(sql);
-																																																																																																																																																																																			
-			//STEP 5: Extract data from result set
-			while(rs.next()){
-				//Retrieve by column name
-				String studentid  = rs.getString("Student_ID");
-				if(studentid == null)
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				String studentid = rs.getString("Student_ID");
+				if (studentid == null)
 					break;
 				String name = rs.getString("Name");
 				String email = rs.getString("Email");
@@ -51,7 +50,7 @@ public class StudentDAO_JDBC implements StudentDAO {
 			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 		// Add exception handling when there is no matching record
-		//return true;
+		// return true;
 		return s;
 	}
 
@@ -59,52 +58,48 @@ public class StudentDAO_JDBC implements StudentDAO {
 	public void getEligibleCourses(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
-			String id = s.getStudentID();
+		try {
 			stmt = dbConnection.createStatement();
 			sql = "select CourseName from Course where SemOffered = " + s.getSem();
 			ResultSet rs = stmt.executeQuery(sql);
 
 			System.out.println("Your eligible courses are\n");
 
-			while(rs.next())
-			{
-				System.out.println("Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
+			while (rs.next()) {
+				System.out.println(
+						"Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
 			}
 
-		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 	}
 
-	@Override 
-	public void viewMyCourses(Student s)
-	{
+	@Override
+	public void viewMyCourses(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
+		try {
 			String id = s.getStudentID();
 			stmt = dbConnection.createStatement();
-			sql = "select CourseName from Course where STUDENT_ID = " + id;//enrollment query
+			sql = "select CourseName from Course where STUDENT_ID = " + id;// enrollment query
 			ResultSet rs = stmt.executeQuery(sql);
 
 			System.out.println("Your courses are\n");
 
-			while(rs.next())
-			{
-				System.out.println("Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
+			while (rs.next()) {
+				System.out.println(
+						"Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
 			}
 
-		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 	}
 
@@ -112,38 +107,36 @@ public class StudentDAO_JDBC implements StudentDAO {
 	public void EnrollForCourse(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
+		try {
 			String id = s.getStudentID();
 			stmt = dbConnection.createStatement();
-			sql = "select CourseName from Course where STUDENT_ID = " + id;//enrollment query
+			sql = "select CourseName from Course where STUDENT_ID = " + id;// enrollment query
 			ResultSet rs = stmt.executeQuery(sql);
 
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		
+
 	}
 
 	@Override
 	public void UnenrollForCourse(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
+		try {
 			String id = s.getStudentID();
 			stmt = dbConnection.createStatement();
-			sql = "select CourseName from Course where STUDENT_ID = " + id;//enrollment query
+			sql = "select CourseName from Course where STUDENT_ID = " + id;// enrollment query
 			ResultSet rs = stmt.executeQuery(sql);
 
-		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 	}
 
@@ -151,57 +144,57 @@ public class StudentDAO_JDBC implements StudentDAO {
 	public void getTranscript(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
+		try {
 			String id = s.getStudentID();
 			stmt = dbConnection.createStatement();
-			sql = "select CourseName from Course where STUDENT_ID = " + id;//enrollment query
+			sql = "select c.CourseName, p.Grade from Course c, Performance p where p.Course_ID = c.Course_ID" + id;// enrollment
+																													// query
 			ResultSet rs = stmt.executeQuery(sql);
 
 			System.out.println("Here is your transcript\n");
 
-			while(rs.next())
-			{
-				System.out.println("Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
+			while (rs.next()) {
+				System.out.println(
+						"Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
 			}
 
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		
+
 	}
 
 	@Override
-	public void viewCoursesbyProf(Student s)
-	{
+	public void viewCoursesbyProf(Student s) {
 		String sql;
 		Statement stmt = null;
-		try{
-			String id = s.getStudentID();
+		try {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter Professor name: ");
+			String name = scanner.nextLine().toLowerCase();
+			scanner.close();
 			stmt = dbConnection.createStatement();
-			sql = "select CourseName from Course where STUDENT_ID = " + id;//enrollment query
+			sql = "select c.Course_ID, c.CourseName from Course c, Professor p where c.Professor_ID = p.Professor_ID and p.Name ="
+					+ name;// enrollment query
+
 			ResultSet rs = stmt.executeQuery(sql);
 
 			System.out.println("Here is your transcript\n");
 
-			while(rs.next())
-			{
-				System.out.println("Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
+			while (rs.next()) {
+				System.out.println(
+						"Course Name = " + rs.getString("CourseName") + "Course_ID = " + rs.getString("Course_ID"));
 			}
 
-		}
-		catch(SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 	}
-
-
-	
 
 }
